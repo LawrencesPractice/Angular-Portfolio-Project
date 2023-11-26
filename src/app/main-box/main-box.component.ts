bbe537eab8256c76a3d18ba4df652c92128cd336
Lawrence Tsui <tbby28@gmail.com>
2023-11-26 18:40:58 +1100

    mainbox

---
+++ src/app/main-box/main-box.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-main-box',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './main-box.component.html',
+  styleUrl: './main-box.component.scss'
+})
+export class MainBoxComponent {
+
+}
--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { MainBoxComponent } from '../main-box/main-box.component';
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MainBoxComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
